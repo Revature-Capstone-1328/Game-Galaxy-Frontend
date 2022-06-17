bcdee310ae3c6de856b65c02df45aaa05effba88
Tanuja Gowrineni <tgowrineni@gmail.com>
2022-06-17 11:36:01 +0200

    game model added

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,2 +1,5 @@
 export class Game {
+    constructor(public gameId int, public name string, public retailPrice double, public releaseDate Date, 
+        public publisher string, public thumb? string){}
+
 }
